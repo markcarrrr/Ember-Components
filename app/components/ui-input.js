c2b306d0d21d123b04a257364baab43765ad4097
Mark Carr <markcarrrr@gmail.com>
2018-05-15 11:19:07 +0100

    mcarr | Added all input types. Added grouped checkboxes. Added 'required' text.

--- app/components/ui-input.js
+++ app/components/ui-input.js
@@ -7,5 +7,23 @@ export default Component.extend({
     type: 'text',
     typeClass: computed(function() {
         return 'form-field--' + this.get('type');
-    })
+    }),
+    init() {
+        this._super(...arguments);
+
+        if (!this.parentView) {
+            return false;
+        }
+
+        const attrs = this.parentView.attrs;
+        const required = attrs.required;
+
+        if (this.get('required') === false) {
+            return false;
+        }
+
+        if (attrs && attrs.required) {
+            this.set('required', required);
+        }
+    }
 });
