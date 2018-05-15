0d0d710ca0df2d20d8650f24a7c1a5e4760e52b7
Mark Carr <markcarrrr@gmail.com>
2018-05-15 15:56:34 +0100

    mcarr | Fix issue with updating options for datepicker. General styling amends.

--- app/components/ui-datepicker.js
+++ app/components/ui-datepicker.js
@@ -1,4 +1,22 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+    classNames: ['datepicker-wrapper', 'form-field'],
+
+    disableDates: computed(function() {
+        return this.get('disable') || [];
+    }),
+
+    enableDates: computed(function() {
+        return this.get('enable') || [];
+    }),
+
+    setAltInput: computed(function() {
+        return this.get('altInput') !== false ? true : false;
+    }),
+
+    setClickOpens: computed(function() {
+        return this.get('clickOpens') !== false ? true : false;
+    })
 });
